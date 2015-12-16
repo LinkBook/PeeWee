ae2a6fda123201a61938aac31dfbf05f5e12dd3a
LinkBook <Hamzezarenasiri@gmail.com>
2015-12-16 11:07:20 +0330

    Materialize

--- WSGI.py
+++ WSGI.py
@@ -11,27 +11,27 @@ from tornado.options import define, options
 from application import *
 from settings import TORNADO_SETTINGS
 
-define("port", default=8000, help="run on the given port", type=int)
+define("port", default=7777, help="run on the given port", type=int)
 """ dastoore taiin porti ke bayad az an goosh kard baraye pasokh dadane request """
 
 if __name__ == '__main__':
 	tornado.options.parse_command_line()
 	app = tornado.web.Application(
 		handlers=[(r'/', MainHandler),
-		          (r'/index', MainsHandler),
-		          (r'/Dashboard', DashBoardHandler),
-		          (r'/about', AboutHandler),
-		          (r'/Contact', ContactHandler),
-		          (r'/help', HelpHandler),
-		          (r'/Login', LoginHandler),
-		          (r'/Logout', LogoutHandler),
-		          (r'/Register', RegisterHandler),
-		          (r'/Webpages', WebpagesHandler),
-		          (r'/webshow', WebshowHandler),
-		          (r"/upload1", UploadFile),
-		          (r'/aa', Ahandler),
-		          (r'/Introduction', IntroductionHandler),
-		          ], **TORNADO_SETTINGS
+				  (r'/index', MainsHandler),
+				  (r'/Dashboard', DashBoardHandler),
+				  (r'/about', AboutHandler),
+				  (r'/Contact', ContactHandler),
+				  (r'/help', HelpHandler),
+				  (r'/Login', LoginHandler),
+				  (r'/Logout', LogoutHandler),
+				  (r'/Register', RegisterHandler),
+				  (r'/Webpages', WebpagesHandler),
+				  (r'/webshow', WebshowHandler),
+				  (r"/upload1", UploadFile),
+				  (r'/aa', Ahandler),
+				  (r'/Introduction', IntroductionHandler),
+				  ], **TORNADO_SETTINGS
 	)
 
 	http_server = tornado.httpserver.HTTPServer(app)
