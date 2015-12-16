73bcdb90df1c6efdde0c8636b3a86507eeeddb42
LinkBook <Hamzezarenasiri@gmail.com>
2015-12-16 11:09:13 +0330

    Materialize

--- WSGI.py
+++ WSGI.py
@@ -8,32 +8,33 @@ import tornado.ioloop
 import tornado.options
 import tornado.web
 from tornado.options import define, options
+
 from application import *
 from settings import TORNADO_SETTINGS
 
-define("port", default=7777, help="run on the given port", type=int)
+define("port", default=8000, help="run on the given port", type=int)
 """ dastoore taiin porti ke bayad az an goosh kard baraye pasokh dadane request """
 
 if __name__ == '__main__':
-	tornado.options.parse_command_line()
-	app = tornado.web.Application(
-		handlers=[(r'/', MainHandler),
-				  (r'/index', MainsHandler),
-				  (r'/Dashboard', DashBoardHandler),
-				  (r'/about', AboutHandler),
-				  (r'/Contact', ContactHandler),
-				  (r'/help', HelpHandler),
-				  (r'/Login', LoginHandler),
-				  (r'/Logout', LogoutHandler),
-				  (r'/Register', RegisterHandler),
-				  (r'/Webpages', WebpagesHandler),
-				  (r'/webshow', WebshowHandler),
-				  (r"/upload1", UploadFile),
-				  (r'/aa', Ahandler),
-				  (r'/Introduction', IntroductionHandler),
-				  ], **TORNADO_SETTINGS
-	)
+    tornado.options.parse_command_line()
+    app = tornado.web.Application(
+        handlers=[(r'/', MainHandler),
+                  (r'/index', MainsHandler),
+                  (r'/Dashboard', DashBoardHandler),
+                  (r'/about', AboutHandler),
+                  (r'/Contact', ContactHandler),
+                  (r'/help', HelpHandler),
+                  (r'/Login', LoginHandler),
+                  (r'/Logout', LogoutHandler),
+                  (r'/Register', RegisterHandler),
+                  (r'/Webpages', WebpagesHandler),
+                  (r'/webshow', WebshowHandler),
+                  (r"/upload1", UploadFile),
+                  (r'/aa', Ahandler),
+                  (r'/Introduction', IntroductionHandler),
+                  ], **TORNADO_SETTINGS
+    )
 
-	http_server = tornado.httpserver.HTTPServer(app)
-	http_server.listen(options.port)
-	tornado.ioloop.IOLoop.instance().start()
+    http_server = tornado.httpserver.HTTPServer(app)
+    http_server.listen(options.port)
+    tornado.ioloop.IOLoop.instance().start()
